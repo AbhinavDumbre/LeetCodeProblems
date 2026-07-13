b3230aa1cb10b1514a875d5e3c9578f28a55d25f
AbhinavDumbre <abhinav.dumbre23@mmit.edu.in>
2026-07-13 10:29:52 +0530

    Time: 1 ms (79.04%), Space: 46.7 MB (63.1%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeDuplicates(int[] arr) {
+      int n=arr.length;
+		int i=0;
+		int j=1;
+		while(j<n) {
+			if(arr[i]==arr[j]) {
+				j++;
+			}
+			else {
+				i++;
+				arr[i]=arr[j];
+				j++;
+			}
+		}return i+1;
+	}
+}
