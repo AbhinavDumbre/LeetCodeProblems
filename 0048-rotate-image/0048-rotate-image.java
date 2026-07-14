21ed107fb7bea3038ee675faa50887aa88795c43
AbhinavDumbre <abhinav.dumbre23@mmit.edu.in>
2026-07-14 20:00:29 +0530

    Time: 0 ms (100%), Space: 43.8 MB (34.61%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int N=matrix.length;
+        for(int i=0;i<N;i++){
+            for(int  j=i+1;j<N;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+
+        } 
+        for(int row=0;row<N;row++){
+            int start=0;
+            int end=N-1;
+            while(start<=end){
+                int temp=matrix[row][start];
+                matrix[row][start]=matrix[row][end];
+                matrix[row][end]=temp;
+                start++;
+                end--;
+            }
+        } 
+    }
+}
