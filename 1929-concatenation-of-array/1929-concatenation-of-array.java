b33ca30af7fe7f086460985d151c0008941ea132
AbhinavDumbre <abhinav.dumbre23@mmit.edu.in>
2026-07-21 16:06:31 +0530

    Time: 8 ms (1.27%), Space: 47.3 MB (37.11%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -1,11 +1,13 @@
 class Solution {
     public int[] getConcatenation(int[] arr) {
         ArrayList<Integer>ar=new ArrayList<>();
-        for(int j=0;j<2;j++){
+        int k=0;
+        while(k<2){
             for(int i=0;i<arr.length;i++){
                 ar.add(arr[i]);
-            }
+            }k++;
     
+        
         }int[] a = ar.stream().mapToInt(Integer::intValue).toArray();return a ;
     }
 }
