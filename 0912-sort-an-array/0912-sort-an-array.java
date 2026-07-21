8537fd948378c8fc47e38a11d14e3f00a4a8b60c
AbhinavDumbre <abhinav.dumbre23@mmit.edu.in>
2026-07-21 15:54:14 +0530

    Time: 21 ms (91.77%), Space: 83.3 MB (52.8%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,17 +1,18 @@
 class Solution {
     public int[] sortArray(int[] arr) {
-      for(int i=0;i<arr.length;i++) {
-		int n=arr.length;
-		int curr=i;
-		int previous=i-1;
-		int currvalue=arr[i];
-		while(previous>=0 && currvalue<arr[previous]) {
-			arr[previous+1]=arr[previous];
-			previous--;
+    //   for(int i=0;i<arr.length;i++) {
+	// 	int n=arr.length;
+	// 	int curr=i;
+	// 	int previous=i-1;
+	// 	int currvalue=arr[i];
+	// 	while(previous>=0 && currvalue<arr[previous]) {
+	// 		arr[previous+1]=arr[previous];
+	// 		previous--;
 			
-		}
+	// 	}
 		
-		arr[previous+1]=currvalue;
-		}return arr;
+	// 	arr[previous+1]=currvalue;}
+    Arrays.sort(arr);
+		return arr;
         }
     }
