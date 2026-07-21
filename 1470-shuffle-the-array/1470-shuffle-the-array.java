08deeb5b0ee114c5e01190cb3eb8fa800973f87b
AbhinavDumbre <abhinav.dumbre23@mmit.edu.in>
2026-07-21 16:36:13 +0530

    Time: 0 ms (100%), Space: 46.5 MB (62.45%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int arr[]=new int[nums.length];
+        int i=0;
+        int m=0;
+        int j=n;
+        while(i<n){
+            if(j<nums.length &&i<n){
+            arr[m]=nums[i];
+            arr[m+1]=nums[j];
+            m++;
+            m++;
+            i++;
+            j++;}
+    
+        }return arr;
+    }
+}
