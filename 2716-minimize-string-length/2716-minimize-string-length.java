3ca1e5b02298bd8169542c1eb79adbc01b8ae62c
AbhinavDumbre <abhinav.dumbre23@mmit.edu.in>
2026-08-04 13:05:36 +0530

    Time: 8 ms (24.57%), Space: 46.8 MB (64.9%) - LeetHub

---
+++ 2716-minimize-string-length/2716-minimize-string-length.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minimizedStringLength(String s) {
+    HashSet <Character>hs=new HashSet<>();
+    for(int i=0;i<s.length();i++){
+        hs.add(s.charAt(i));
+    }
+    int ans=hs.size();
+
+    return ans;
+    }
+}
