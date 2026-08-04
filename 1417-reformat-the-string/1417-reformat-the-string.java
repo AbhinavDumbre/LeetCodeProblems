f0a3f824358767acd563c54d64fa5d20d3e74aec
AbhinavDumbre <abhinav.dumbre23@mmit.edu.in>
2026-08-04 12:46:46 +0530

    Time: 6 ms (31.19%), Space: 44.6 MB (84.71%) - LeetHub

---
+++ 1417-reformat-the-string/1417-reformat-the-string.java
@@ -0,0 +1,55 @@
+class Solution {
+    public String reformat(String s) {
+		StringBuffer letter = new StringBuffer();
+		StringBuffer digit = new StringBuffer();
+		for (char i : s.toCharArray()) {
+			if (Character.isLetter(i)) {
+				letter.append(i);
+
+			}
+			if (Character.isDigit(i)) {
+				digit.append(i);
+			}
+
+		}
+		if (Math.abs(letter.length() - digit.length()) > 1) {
+    return "";
+}
+
+      
+		StringBuilder ans = new StringBuilder();
+
+		int i = 0;
+		int j = 0;
+		if (digit.length() > letter.length()) {
+			while (i < digit.length() || j < letter.length()) {
+				if (i < digit.length()) {
+					ans.append(digit.charAt(i));
+					i++;
+				}
+				if (j < letter.length()) {
+					ans.append(letter.charAt(j));
+					j++;
+
+				}
+			}
+
+		}
+		if (digit.length() <= letter.length()) {
+			while (i < letter.length() || j < digit.length()) {
+				if (i < letter.length()) {
+					ans.append(letter.charAt(i));
+					i++;
+				}
+				if (j < digit.length()) {
+					ans.append(digit.charAt(j));
+					j++;
+
+				}
+			}
+
+		}
+        
+		return ans.toString();
+	}
+}
