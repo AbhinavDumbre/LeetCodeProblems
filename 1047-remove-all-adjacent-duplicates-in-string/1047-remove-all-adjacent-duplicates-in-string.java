456c54543f7145055031774641a2b98e9446b72e
AbhinavDumbre <abhinav.dumbre23@mmit.edu.in>
2026-08-05 14:59:15 +0530

    Time: 13 ms (80.9%), Space: 46.2 MB (97.01%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public String removeDuplicates(String s) {
-        StringBuffer br=new StringBuffer();
+        StringBuilder br=new StringBuilder();
         for(int i=0;i<s.length();i++){
         char ch=s.charAt(i);
         if(br.length()>0 && br.charAt(br.length()-1)==ch){
@@ -11,5 +11,10 @@ class Solution {
         }
         }return br.toString();
 
+    // HashSet<Character>hs=new HashSet<>();
+    // for(char i : s.toCharArray()){
+    //     hs.add(i);
+    // }
+    // return hs.toString();
     }
 }
